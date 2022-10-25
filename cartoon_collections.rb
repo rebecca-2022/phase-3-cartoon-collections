0aef6a9bc72b037e32eb8d017855b2cb8e00829a
rebecca njeri <rebecca.njeri@student.moringaschool.com>
2022-10-25 13:38:17 +0300

    complete assignment

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves arr
   # Your code here
+  arr.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet arr
   # Your code here
+  arr.map do |name|
+    name.capitalize << "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls arr
   # Your code here
+  arr.length > 4 ? false : true
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese arr
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  if arr.include?("cheddar" || "gouda" || "camembert")
+    return arr.detect do |cheese| 
+      cheese == ("cheddar" || "gouda" || "camembert")
+    end
+  else
+    return nil
+  end
 end
